acfebde217db7e7fade63b7c79b1480f0e0bb262
seahrh <seahrh@gmail.com>
2019-04-12 16:43:45 +0800

    trip average speed: tests 400 and 404

--- test/v1/trip/TripAverageSpeedSpec.scala
+++ test/v1/trip/TripAverageSpeedSpec.scala
@@ -13,20 +13,55 @@ class TripAverageSpeedSpec extends PlaySpec with GuiceOneAppPerSuite {
   implicit lazy val materializer: Materializer = app.materializer
 
   "Trip Average Speed" should {
-    "return 200 OK and data when data exists" in {
+    "return 200 OK and resource when resource exists" in {
       val request = FakeRequest(GET, "/average_speed_24hrs?date=2019-04-01")
         .withHeaders(HOST -> "localhost:8080")
-      route(app, request) match {
-        case Some(res) =>
-          status(res) mustEqual OK
-          //contentAsString(res) mustEqual ""
-          //contentAsJson(res) mustEqual Json.obj()
-          val payload: SuccessPayload = contentAsJson(res).as[SuccessPayload]
-          val data: Seq[AverageSpeedResource] = payload.data.as[Seq[AverageSpeedResource]]
-          data.isEmpty mustEqual false
-        case _ => throw new IllegalStateException("invalid route")
+      for (res <- route(app, request)) {
+        status(res) mustEqual OK
+        val payload: SuccessPayload = contentAsJson(res).as[SuccessPayload]
+        val data: Seq[AverageSpeedResource] = payload.data.as[Seq[AverageSpeedResource]]
+        data.isEmpty mustEqual false
       }
     }
+
+    "return 404 Not Found and details when resource does not exist" in {
+      val request = FakeRequest(GET, "/average_speed_24hrs?date=2019-04-06")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual NOT_FOUND
+        val payload: FailurePayload = contentAsJson(res).as[FailurePayload]
+        payload.error.message must startWith("Average speed not found")
+      }
+    }
+
+    "return 404 Not Found when path does not exist" in {
+      val request = FakeRequest(GET, "/bad_path")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual NOT_FOUND
+      }
+    }
+
+    "return 400 Bad Request and details when input value is invalid" in {
+      val request = FakeRequest(GET, "/average_speed_24hrs?date=2019-02-31")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual BAD_REQUEST
+        val payload: FailurePayload = contentAsJson(res).as[FailurePayload]
+        payload.error.message must startWith("Invalid date")
+      }
+    }
+
+    "return 400 Bad Request when input key is invalid" in {
+      val request = FakeRequest(GET, "/average_speed_24hrs?name=2019-02-01")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual BAD_REQUEST
+        contentAsString(res) mustEqual "Missing parameter: date"
+      }
+    }
+
+
   }
 
 }

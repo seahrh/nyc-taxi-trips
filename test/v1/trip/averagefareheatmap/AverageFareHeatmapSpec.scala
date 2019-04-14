462fc0b47b25dc5eb54a4d2ce313f2246bfa2492
seahrh <seahrh@gmail.com>
2019-04-14 12:00:33 +0800

    test average fare heatmap endpoint

---
+++ test/v1/trip/averagefareheatmap/AverageFareHeatmapSpec.scala
@@ -0,0 +1,61 @@
+package v1.trip.averagefareheatmap
+
+import akka.stream.Materializer
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.test.Helpers._
+import play.api.test._
+import v1._
+
+class AverageFareHeatmapSpec extends PlaySpec with GuiceOneAppPerSuite {
+
+  implicit lazy val materializer: Materializer = app.materializer
+
+  private val baseUrl: String = "/average_fare_heatmap"
+
+  "Average Fare Heatmap" should {
+
+    "return 200 OK and resource when resource exists" in {
+      val request = FakeRequest(GET, s"$baseUrl?date=2019-04-01")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual OK
+        val payload: SuccessPayload = contentAsJson(res).as[SuccessPayload]
+        val data: Seq[Resource] = payload.data.as[Seq[Resource]]
+        data.isEmpty mustEqual false
+      }
+    }
+
+    "return 404 Not Found and details when resource does not exist" in {
+      val request = FakeRequest(GET, s"$baseUrl?date=2019-04-06")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual NOT_FOUND
+        val payload: FailurePayload = contentAsJson(res).as[FailurePayload]
+        payload.error.message must startWith("Average speed not found")
+      }
+    }
+
+    "return 400 Bad Request and details when input value is invalid" in {
+      val request = FakeRequest(GET, s"$baseUrl?date=2019-02-31")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual BAD_REQUEST
+        val payload: FailurePayload = contentAsJson(res).as[FailurePayload]
+        payload.error.message must startWith("Invalid date")
+      }
+    }
+
+    "return 400 Bad Request and details when input key is invalid" in {
+      val request = FakeRequest(GET, s"$baseUrl?name=2019-02-01")
+        .withHeaders(HOST -> "localhost:8080")
+      for (res <- route(app, request)) {
+        status(res) mustEqual BAD_REQUEST
+        contentAsString(res) mustEqual "Missing parameter: date"
+      }
+    }
+
+
+  }
+
+}
